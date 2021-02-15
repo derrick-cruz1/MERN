eda33b3b66204b9150d6e0320948bb39cbb3f2e9
Derrick <derrick.cruz1@gmail.com>
2021-02-15 13:17:56 -0800

    FirstJSX_Assignment Final

---
+++ firstJSX_Assignment/first-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className = "dojo">
+      <h1>Hello Dojo!</h1>
+        <lu>Things I need to do:
+          <li>Learn React.js</li>
+          <li>Climb Mt. Everest </li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </lu>
+      </div>
+    </div>
+  );
+}
+
+export default App;
