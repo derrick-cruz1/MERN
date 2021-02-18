0d5c6296124e5cc3de041d6d2dee813dbf145d45
Derrick <derrick.cruz1@gmail.com>
2021-02-17 18:04:18 -0800

    Final for putting it all together

--- propItUp_Assignment/prop/src/components/PersonCard.js
+++ propItUp_Assignment/prop/src/components/PersonCard.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            age: this.props.age
+        };
+    }
+    
+    
+    
     render(){
         return(
             <div classname = "app">
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
                 <p>Age: {this.props.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={() => this.setState({age: this.state.age + 1 })}> Birthday button for {this.props.firstName} {this.props.lastName} </button>
             </div>
         );
     }
