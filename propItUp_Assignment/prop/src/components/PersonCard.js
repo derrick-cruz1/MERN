6c343f38930b264959f297ab0c7acc198001e284
Derrick <derrick.cruz1@gmail.com>
2021-02-19 11:46:03 -0800

    Final Inversion

--- propItUp_Assignment/prop/src/components/PersonCard.js
+++ propItUp_Assignment/prop/src/components/PersonCard.js
@@ -1,25 +1,13 @@
 import React, {Component} from 'react';
 
-class PersonCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state ={
-            age: this.props.age
-        };
-    }
-    
-    
-    
-    render(){
-        return(
-            <div classname = "app">
-                <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
-                <p>Hair Color: {this.props.hairColor}</p>
-                <button onClick={() => this.setState({age: this.state.age + 1 })}> Birthday button for {this.props.firstName} {this.props.lastName} </button>
-            </div>
-        );
-    }
+const PersonCard = props => {
+    return(
+        <div>
+            <h1>{ props.lastName }, { props.firstName }</h1>
+            <p>Age: { props.age }</p>
+            <p>Hair Color: { props.hairColor }</p>
+        </div>
+    );
 }
 
 export default PersonCard;
