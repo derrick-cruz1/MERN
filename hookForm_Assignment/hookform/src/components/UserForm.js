3deaf2e889480f185b40ea1acf02bf6544371442
Derrick <derrick.cruz1@gmail.com>
2021-02-23 13:12:29 -0800

    Final for more forms

--- hookForm_Assignment/hookform/src/components/UserForm.js
+++ hookForm_Assignment/hookform/src/components/UserForm.js
@@ -18,29 +18,66 @@ const UserForm = (props) => {
         <form onSubmit = {createUser}>
             <div>
                 <label>First Name: </label>
-                <input type="text" onChange={(e)=> setFirst(e.target.value)} />
+                <input type="text"
+                    name = "firstName"
+                    value = {firstName}
+                    onChange={(e)=> setFirst(e.target.value)} 
+                />
+                {
+                    firstName.length < 1 ?
+                    <span>First Name must be at least 2 characters or more.</span>
+                    : null
+                }
             </div>
             <div>
                 <label>Last Name: </label>
-                <input type="text" onChange={(e)=> setLast(e.target.value)} />
+                <input type="text"
+                    name = "lastName"
+                    value = {lastName}
+                    onChange={(e)=> setLast(e.target.value)} />
+                {
+                    lastName.length < 1 ?
+                    <span>Last Name must be at least 2 characters or more.</span>
+                    : null
+                }
             </div>
             <div>
                 <label>Email: </label>
-                <input type="text" onChange={(e)=> setEmail(e.target.value)} />
+                <input type="text" 
+                name = "email"
+                value = {email}
+                onChange={(e)=> setEmail(e.target.value)} />
+                {
+                   email.length < 5 ?
+                   <span>Email must be at least 5 characters or more.</span>
+                   : null 
+                }
             </div>
             <div>
                 <label>Password: </label>
-                <input type="text" onChange={(e)=> setPassword(e.target.value)} />
+                <input 
+                type="text" 
+                name = "email"
+                value = {password}
+                onChange={(e)=> setPassword(e.target.value)} />
+                {
+                    password.length < 8 ?
+                    <span>Password must be at least 8 characters or more.</span>
+                    : null 
+                }
             </div>
             <div>
                 <label>Confirm Password: </label>
-                <input type="text" onChange={(e)=> setConfirmPassword(e.target.value)} />
+                <input type="text" 
+                name = "confirmPassword"
+                value = {confirmPassword}
+                onChange={(e)=> setConfirmPassword(e.target.value)} />
+                {
+                     password != confirmPassword ?
+                     <span>Password must match!</span>
+                     : null 
+                }
             </div>
-            <p>Your form data:</p>
-            <p>First name: {firstName}</p>
-            <p>Last name: {lastName}</p>
-            <p>Email: {email}</p>
-            <p>Password: {password}</p>
         </form>
         
     );
